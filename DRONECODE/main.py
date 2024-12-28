a023b7813e0307f771300da20df0403b8c26188a
AnnieL5 <alian5@ocdsb.ca>
2024-12-28 16:35:46 -0500

    Added ki for PID - Tested & value needs to be adjusted

--- DRONECODE/main.py
+++ DRONECODE/main.py
@@ -5,6 +5,8 @@ from MPU_data import MPU6050DATA
 # From 12.21
 # - with a while loop
 # - detects angle instead of angular speed
+# Future: add error value. To be controller - gyro data
+# 		  add pid for yaw & kd
 
 led = Pin(25, Pin.OUT)
 
@@ -21,6 +23,7 @@ esc_3.freq(50)
 esc_4.freq(50)
 
 period_ms = 20
+max_throttle = int ((2/ period_ms) * 65535)
 min_throttle = int ((1/ period_ms) * 65535)
 goal_throttle = int ((1.34/ period_ms) * 65535)
 add_throttle = int ((0.03/ period_ms) * 65535)
@@ -28,7 +31,7 @@ add_throttle = int ((0.03/ period_ms) * 65535)
 steps = 100  # Define the number of steps
 duty_step = (goal_throttle - min_throttle) // steps
 
-maxThrottleDuration = 3 * 1000 # in ms
+maxThrottleDuration = 5 * 1000 # in ms
 hasTilted = False
 
 on = True
@@ -39,14 +42,23 @@ endHoverTime = 0
 
 # PID Controller values
 pid_roll_kp:float = 4.5
-# pid_roll_ki:float = 0.00255
+pid_roll_ki:float = 0.002
 # pid_roll_kd:float = 0.00002571429
 pid_pitch_kp:float = pid_roll_kp
-# pid_pitch_ki:float = pid_roll_ki
+pid_pitch_ki:float = pid_roll_ki
 # pid_pitch_kd:float = pid_roll_kd
 pid_yaw_kp:float = pid_roll_kp
-# pid_yaw_ki:float = 0.003428571
+pid_yaw_ki:float = 0.003428571
 # pid_yaw_kd:float = 0.0
+
+i_limit = 30
+
+roll_last_integral:float = 0.0
+#roll_last_error:float = 0.0
+pitch_last_integral:float = 0.0
+#pitch_last_error:float = 0.0
+yaw_last_integral:float = 0.0
+# yaw_last_error:float = 0.0
 
 transition_throttle = int ((1.1/ period_ms) * 65535) # when it switch from motor set duty_cycle to adding pid values
 
@@ -61,6 +73,13 @@ def setThrottle(throttle) -> None:
     esc_2.duty_u16(duty_cycle)
     esc_3.duty_u16(duty_cycle)
     esc_4.duty_u16(duty_cycle)
+    
+def constrainThrottle(t1, t2, t3, t4):
+    t1 = max(min(t1, max_throttle), min_throttle) # constrain within throttle limits
+    t2 = max(min(t2, max_throttle), min_throttle) # constrain within throttle limits
+    t3 = max(min(t3, max_throttle), min_throttle) # constrain within throttle limits
+    t4 = max(min(t4, max_throttle), min_throttle) # constrain within throttle limits
+    return t1,t2,t3,t4
 
 try:
 
@@ -80,10 +99,27 @@ try:
         # led.value(0)
         
         angle = mpu.getAngle()
-        t1:int = int(duty_cycle - pid_pitch_kp*angle[0] - pid_roll_kp*angle[1]) # - pid_yaw_kp*angle[2] 
-        t2:int = int(duty_cycle + pid_pitch_kp*angle[0] - pid_roll_kp*angle[1]) # + pid_yaw_kp*angle[2] 
-        t3:int = int(duty_cycle + pid_pitch_kp*angle[0] + pid_roll_kp*angle[1]) # - pid_yaw_kp*angle[2] 
-        t4:int = int(duty_cycle - pid_pitch_kp*angle[0] + pid_roll_kp*angle[1]) # + pid_yaw_kp*angle[2] 
+        
+        # pitch PID calc
+        pitch_p:float = angle[0] * pid_pitch_kp
+        pitch_i:float = pitch_last_integral + (angle[0] * pid_pitch_ki)
+        pitch_i = max(min(pitch_i, i_limit), -i_limit) # constrain within I-term limits
+        # pitch_d:float = pid_pitch_kd * (error_rate_pitch - pitch_last_error) / cycle_time_seconds
+        pid_pitch = pitch_p + pitch_i # + pitch_d
+        
+         # roll PID calc
+        roll_p:float = angle[1] * pid_roll_kp
+        roll_i:float = roll_last_integral + (angle[1] * pid_roll_ki)
+        roll_i = max(min(roll_i, i_limit), -i_limit) # constrain within I-term limits
+        # roll_d:float = pid_roll_kd * (error_rate_roll - roll_last_error) / cycle_time_seconds
+        pid_roll:float = roll_p + roll_i # + roll_d
+        
+        t1:int = int(duty_cycle - pid_pitch - pid_roll) # - pid_yaw_kp*angle[2]
+        t2:int = int(duty_cycle + pid_pitch - pid_roll) # + pid_yaw_kp*angle[2] 
+        t3:int = int(duty_cycle + pid_pitch + pid_roll) # - pid_yaw_kp*angle[2] 
+        t4:int = int(duty_cycle - pid_pitch + pid_roll) # + pid_yaw_kp*angle[2]
+        
+        t1,t2,t3,t4 = constrainThrottle(t1,t2,t3,t4)
 
         if takeOff: #while taking off
 
@@ -132,6 +168,14 @@ try:
         
             if(ticks_diff(endHoverTime, ticks_ms()) <= 0): # check how long it hovered
                 landing = True # change to landing if hovered for enough time
+        
+        # Save state values for next loop
+#         roll_last_error = error_rate_roll
+#         pitch_last_error = error_rate_pitch
+#         yaw_last_error = error_rate_yaw
+        roll_last_integral = roll_i
+        pitch_last_integral = pitch_i
+        #yaw_last_integral = yaw_i
         
         #Stop all motors if it tilts
         #mpu.readData()
